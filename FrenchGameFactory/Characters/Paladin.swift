c341a3ceab80f465515e1220a4b5f0d800dbf406
Namsco <naim.zidouni@outlook.fr>
2022-10-22 13:11:26 +0200

    Create paladin class

--- FrenchGameFactory/Characters/Paladin.swift
+++ FrenchGameFactory/Characters/Paladin.swift
@@ -7,3 +7,10 @@
 
 import Foundation
 
+class Paladin: Characters {
+    init() {
+        super.init(className: "Paladin", characterHealth: 6, weaponName: "Spear", weaponDamage: 2, talent: 0.5)
+    }
+}
+var paladin = Paladin()
+
