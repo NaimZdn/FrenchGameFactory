306125ee4680f19e37083a16675686f431c14bbd
Namsco <naim.zidouni@outlook.fr>
2022-10-23 22:55:51 +0200

    Added 3 prints to test the game

---
+++ FrenchGameFactory/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  FrenchGameFactory
+//
+//  Created by Zidouni on 22/10/2022.
+//
+
+import Foundation
+
+print(team.Player1Pseudo())
+print(team.Player2Pseudo())
+print(team.chooseYourTeam())
+
