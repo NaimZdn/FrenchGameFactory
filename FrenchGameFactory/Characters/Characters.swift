6a1a878967f5b3fa3e264dd836c9de425823f7c5
Namsco <naim.zidouni@outlook.fr>
2022-10-24 15:53:26 +0200

    Added characterName var inside  the Characters class

--- FrenchGameFactory/Characters/Characters.swift
+++ FrenchGameFactory/Characters/Characters.swift
@@ -8,17 +8,18 @@
 import Foundation
 class Characters {
     var className: String;
+    var characterName: String;
     var characterHealth: Double;
     var weaponName: String;
     var weaponDamage: Double;
     var talent: Double;
     
-    init(className: String, characterHealth: Double, weaponName: String, weaponDamage: Double, talent: Double) {
+    init(className: String, characterName: String, characterHealth: Double, weaponName: String, weaponDamage: Double, talent: Double) {
         self.className = className;
+        self.characterName = characterName
         self.characterHealth = characterHealth;
         self.weaponName = weaponName;
         self.weaponDamage = weaponDamage;
         self.talent = talent;
     }
 }
-
