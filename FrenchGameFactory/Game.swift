5d28ff5aa94bf91ae8781a7506297201b6b7f0aa
Namsco <naim.zidouni@outlook.fr>
2022-10-25 12:53:58 +0200

    Added class game with startingMenu function

---
+++ FrenchGameFactory/Game.swift
@@ -0,0 +1,21 @@
+//
+//  Game.swift
+//  FrenchGameFactory
+//
+//  Created by Zidouni on 25/10/2022.
+//
+
+import Foundation
+class Game {
+    
+    func startingMenu() {
+        print("Hello and welcome on the FrenchFactory company's game specially create for you.\n Let me tell you there rules : 📜 \n "
+            + "\n1. You must form a team of 3 characters. You can't have 2 same characters in your Team. \n"
+            + "\n2. Each character has a talent with 50% chance's activation. \n"
+            + "\n3. You fight between fighters. The winner is the one who killed the opposing team's fighters. \n"
+            + "\n4. The last rule and the more important, enjoy the game 😁. \n")
+        print("------------------------------------------------------")
+    }
+}
+
+var game = Game()
