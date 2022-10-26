5c0de7bbef33f3bdc4f281cb4275021f22405bb9
Namsco <naim.zidouni@outlook.fr>
2022-10-26 13:02:06 +0200

    Added expression controller when the player choose his name

--- FrenchGameFactory/Player/Player.swift
+++ FrenchGameFactory/Player/Player.swift
@@ -8,23 +8,30 @@
 import Foundation
 class Player {
     var playerPseudo: [String] = [];
-    
     func player1Pseudo() -> String {
         var pseudo = ""
         repeat {
             print("Hello Player 1 ! \n Please choose your Pseudo : ")
-            if let pseudoData = readLine() {
+          
+            if let pseudoData:String = readLine() {
                 pseudo = pseudoData
-                if playerPseudo.contains(pseudo) {
+                if pseudo.count > 10 || pseudo.count < 1 || pseudo.contains(" "){
+                    print("Your pseudo must contain at minimum 1 letter and at maximum 10 letters.Spaces has'nt accept. ")
+                    print("------------------------------------------------------")
+                    return player1Pseudo();
+                } else if playerPseudo.contains(pseudo) {
                     print("This pseudo \(pseudo) is already use, please choose an another")
-                    player1Pseudo();
-                } else {
+                    print("------------------------------------------------------")
+                    return player1Pseudo();
+                } else if !playerPseudo.contains(pseudo){
                     playerPseudo.append(pseudo)
                     print("Welcome \(pseudo) !")
+                    print("------------------------------------------------------")
                 }
+
             }
         } while pseudo == ""
-        return ""
+        return pseudo
     }
     
     func player2Pseudo() -> String {
@@ -33,16 +40,22 @@ class Player {
             print("Hello Player 2 ! \n Please choose your Pseudo : ")
             if let pseudoData = readLine() {
                 pseudo = pseudoData
-                if playerPseudo.contains(pseudo) {
+                if pseudo.count > 10 || pseudo.count < 1 || pseudo.contains(" "){
+                    print("Your pseudo must contain at minimum 1 letter and at maximum 10 letters. Spaces has'nt accept. ")
+                    print("------------------------------------------------------")
+                    return player1Pseudo();
+                } else if playerPseudo.contains(pseudo) {
                     print("This pseudo \(pseudo) is already use, please choose an another")
-                    player2Pseudo();
+                    print("------------------------------------------------------")
+                    return player2Pseudo();
                 } else {
                     playerPseudo.append(pseudo)
                     print("Welcome \(pseudo) !")
+                    print("------------------------------------------------------")
                 }
             }
         } while pseudo == ""
-        return ""
+        return pseudo
     }
 }
 
