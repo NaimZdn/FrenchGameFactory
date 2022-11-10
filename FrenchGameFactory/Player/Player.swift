c6abf4dcb6f03f976d338a344c1d0c00ae7d0029
Namsco <naim.zidouni@outlook.fr>
2022-11-10 16:37:42 +0100

    Add best practices for Player class - Remove global instance for Player class

--- FrenchGameFactory/Player/Player.swift
+++ FrenchGameFactory/Player/Player.swift
@@ -7,59 +7,45 @@
 
 import Foundation
 class Player {
-    var playerPseudo: [String] = [];
-    func player1Pseudo() -> String {
-        var pseudo = ""
+    
+    var player: String
+    var pseudo: String
+    
+    init(player: String) {
+        self.player = player
+        self.pseudo = ""
+    }
+    
+    func choosePlayerPseudo(allPlayerPseudo: [String]) -> String {
+        var playerPseudo = ""
         repeat {
-            print("Hello Player 1 ! \n Please choose your Pseudo : ")
-          
-            if let pseudoData:String = readLine() {
-                pseudo = pseudoData
-                if pseudo.count > 10 || pseudo.count < 1 || pseudo.contains(" "){
+            print("Hello \(player) ! \n Please choose your Pseudo : ")
+            
+            if let pseudoData = readLine() {
+                playerPseudo = pseudoData
+                if playerPseudo.count > 10 || playerPseudo.count < 1 || playerPseudo.contains(" "){
                     print("=============================================================")
                     print("Your pseudo must contain at minimum 1 letter and at maximum 10 letters.Spaces has'nt accept. ")
                     print("=============================================================")
-                    return player1Pseudo();
-                } else if playerPseudo.contains(pseudo) {
+                    
+                } else if allPlayerPseudo.contains(playerPseudo) {
                     print("=============================================================")
-                    print("This pseudo \(pseudo) is already use, please choose an another")
+                    print("This pseudo \(playerPseudo) is already use, please choose an another")
                     print("=============================================================")
-                    return player1Pseudo();
-                } else if !playerPseudo.contains(pseudo){
-                    playerPseudo.append(pseudo)
-                    print("=============================================================")
-                    print("Welcome \(pseudo) !")
-                    print("=============================================================")
-                }
-
-            }
-        } while pseudo == ""
-        return pseudo
-    }
-    
-    func player2Pseudo() -> String {
-        var pseudo = ""
-        repeat {
-            print("Hello Player 2 ! \n Please choose your Pseudo : ")
-            if let pseudoData = readLine() {
-                pseudo = pseudoData
-                if pseudo.count > 10 || pseudo.count < 1 || pseudo.contains(" "){
-                    print("Your pseudo must contain at minimum 1 letter and at maximum 10 letters. Spaces has'nt accept. ")
-                    print("=============================================================")
-                    return player1Pseudo();
-                } else if playerPseudo.contains(pseudo) {
-                    print("This pseudo \(pseudo) is already use, please choose an another")
-                    print("=============================================================")
-                    return player2Pseudo();
+                    
                 } else {
-                    playerPseudo.append(pseudo)
-                    print("Welcome \(pseudo) !")
                     print("=============================================================")
+                    print("Welcome \(playerPseudo) !")
+                    print("=============================================================")
+                    pseudo = playerPseudo
+                
                 }
             }
+
         } while pseudo == ""
         return pseudo
+                
     }
 }
 
-var player = Player()
+
