9e7dd6896ec1663dc1156e400f5437f0c7b233d7
Namsco <naim.zidouni@outlook.fr>
2022-11-14 00:11:37 +0100

    instantiation of game and added starting menu

--- FrenchGameFactory/main.swift
+++ FrenchGameFactory/main.swift
@@ -6,16 +6,7 @@
 //
 
 import Foundation
-// initialiser le jeu
-// function qui renvoie un bool qui va test si le jeu est fini ou non (dans game)
 
-//var game = Game(attackerTeam1: magus, attackerTeam2: magus, attackedTeam1: magus, attackedTeam2: magus)
 var game = Game()
 game.startingMenu()
 
-/*while true {
-    game.turn += 1
-    print("============================ TURN \(game.turn) ============================")
-    game.fightingTeam1()
-    game.fightingTeam2()
-}*/
