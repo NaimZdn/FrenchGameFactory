14910279553296f9e6f062cfd6c0ea8c22e865cb
Namsco <naim.zidouni@outlook.fr>
2022-11-29 11:41:07 +0100

    Added an override for attack function to describe specific case for Warrior and Druid character.

--- FrenchGameFactory/Characters/Druid.swift
+++ FrenchGameFactory/Characters/Druid.swift
@@ -11,4 +11,15 @@ class Druid: Characters{
     init(characterName: String) {
         super.init(className: "Druid 🦌", characterDescription: "\n4.🦌 a Druid with 6 life points. His weapon is a Stick which inflicts 1 point of damage. With his talent the druid can heal one ally.", characterName: characterName, characterHealth: 6, weaponName: "Stick", weaponDamage: 1, talent: 1)
     }
+    override func talentActivation(attackedCharacter: Characters, attackerCharacter: Characters, playerTeam: Team) {
+        if attackerCharacter.className == className { //  If the character attacker is a Druid his talent healed a team character's choosing by the player with healingteam function.
+            print("=============================================================")
+            print("Oh your \(className) active his talent, you can choose a character to heal : ")
+            print("=============================================================")
+            playerTeam.statsTeam()
+            super.healCharacter(characterHealer: attackerCharacter, playerTeam: playerTeam)
+            attackedCharacter.characterHealth -= attackerCharacter.weaponDamage
+            
+        }
+    }
 }
--- FrenchGameFactory/Characters/Warrior.swift
+++ FrenchGameFactory/Characters/Warrior.swift
@@ -11,4 +11,14 @@ class Warrior: Characters {
     init(characterName: String) {
         super.init(className: "Warrior 🛡",characterDescription: "\n1.🛡 a Warrior with 10 life points. His weapon is a Sword which inflicts 1 point of damage. With his talent the warrior use his shield for protect hiself, the shield reduce 0.5 point of the opponent damages.", characterName: characterName, characterHealth: 10, weaponName: "Sword", weaponDamage: 1, talent: 0.5)
     }
+    override func talentActivation(attackedCharacter: Characters, attackerCharacter: Characters, playerTeam: Team) {
+        if attackedCharacter.className == className { // If the character attacked is a Warrior his talent is reducing damage to 0.5 point.
+            print("=============================================================")
+            print("Oh ! Opponent Warrior's 🛡 make a shield for reduce your attack")
+            print("=============================================================")
+            let damageReduce: Double = attackerCharacter.weaponDamage - attackedCharacter.talent
+            attackedCharacter.characterHealth -= damageReduce
+            
+        }
+    }
 }
