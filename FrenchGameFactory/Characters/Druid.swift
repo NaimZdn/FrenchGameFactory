c0f01508041b48a75b0ce95994590129cf633c4d
Namsco <naim.zidouni@outlook.fr>
2022-11-30 19:45:44 +0100

    Added healCharacter() function inside Druid class.

--- FrenchGameFactory/Characters/Druid.swift
+++ FrenchGameFactory/Characters/Druid.swift
@@ -11,15 +11,26 @@ class Druid: Characters{
     init(characterName: String) {
         super.init(className: "Druid 🦌", characterDescription: "\n4.🦌 a Druid with 6 life points. His weapon is a Stick which inflicts 1 point of damage. With his talent the druid can heal one ally.", characterName: characterName, characterHealth: 6, weaponName: "Stick", weaponDamage: 1, talent: 1)
     }
-    override func talentActivation(attackedCharacter: Characters, attackerCharacter: Characters, playerTeam: Team) {
-        if attackerCharacter.className == className { //  If the character attacker is a Druid his talent healed a team character's choosing by the player with healingteam function.
+    override func talentActivation(attackedCharacter: Characters, playerTeam: Team) {
+         //  If the character attacker is a Druid his talent healed a team character's choosing by the player with healingteam function.
             print("=============================================================")
             print("Oh your \(className) active his talent, you can choose a character to heal : ")
             print("=============================================================")
             playerTeam.statsTeam()
-            super.healCharacter(characterHealer: attackerCharacter, playerTeam: playerTeam)
-            attackedCharacter.characterHealth -= attackerCharacter.weaponDamage
-            
-        }
+            healCharacter(playerTeam: playerTeam)
+            attackedCharacter.characterHealth -= weaponDamage
+    }
+    
+    // We create a function for the druid's talent
+    func healCharacter(playerTeam: Team) {
+        var healing: Int = 0
+        repeat {
+            let characterSelected = playerTeam.characterSelection() // We call the function to permit player to select a character who receive the heal.
+            print("=============================================================")
+            print("Your \(characterSelected.className) gain \(talent) lifepoint")
+            print("=============================================================")
+            characterSelected.characterHealth += talent
+            healing = 1
+        } while healing != 1
     }
 }
